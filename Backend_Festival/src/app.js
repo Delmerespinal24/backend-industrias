6fc8bdf88c3fde56c2a6685414edae5b132c721f
The-Running-Riot <101164427+The-Running-Riot@users.noreply.github.com>
2023-03-06 23:08:50 -0600

    cambio app

--- Backend_Festival/src/app.js
+++ Backend_Festival/src/app.js
@@ -17,10 +17,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 // Routes
