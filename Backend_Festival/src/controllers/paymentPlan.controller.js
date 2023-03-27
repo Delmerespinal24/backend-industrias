1e8f8f23853737a7f6dd231f2eb47331b47ded3b
carlosalgadoa <75154640+carlosalgadoa@users.noreply.github.com>
2023-03-27 13:28:14 -0600

    Revisar API payment nuevos campos

--- Backend_Festival/src/controllers/paymentPlan.controller.js
+++ Backend_Festival/src/controllers/paymentPlan.controller.js
@@ -3,7 +3,7 @@ import { getConnection } from "../database/database";
 
 const addCard = async (req, res) => {
     const pool = await getConnection();
-    //await pool.query('start transaction')
+    
     try {
        
         var tarjeta={
@@ -12,10 +12,19 @@ const addCard = async (req, res) => {
             codigoCVV:req.body.codigoCVV
         }
 
+        var plan={
+            tipoPlan:req.body.tipoPlan,
+            fechaInicio : "",
+            fechaFin:req.body.fechaFin,
+            precio:req.body.precio,
+            idUsuario:req.body.idUsuario
+        }
+
         var status=200;
         var message;
 
-        if (tarjeta.noTarjeta === undefined || tarjeta.fechaVencimientoT === undefined || tarjeta.codigoCVV === undefined) {
+        if (//plan.tipoPlan === undefined ||plan.fechaFin === undefined ||plan.precio === undefined ||
+            tarjeta.noTarjeta === undefined || tarjeta.fechaVencimientoT === undefined || tarjeta.codigoCVV === undefined) {
              return res.status(400).json({
                  "message": "¡Advertencia! Por favor llenar todos los campos."
              });
@@ -49,6 +58,18 @@ const addCard = async (req, res) => {
         }
 
        else{
+
+         //OBTENER LA FECHAINICIO (EN QUE SE COMPRÓ EL PLAN)
+         let hoy = new Date();
+         let dia = hoy.getDate();
+         let mes = hoy.getMonth() + 1;
+         let anio = hoy.getFullYear();
+
+        plan.fechaInicio = `${anio}-${mes}-${dia}`; 
+
+        //INSERTAR DATOS EN PLANES
+        await pool.query("INSERT INTO planes SET ?",plan); 
+
         var message="Pago realizado con exito!";
        }
         
@@ -60,6 +81,7 @@ const addCard = async (req, res) => {
             return;
 
     } catch (error) {
+       
         res.status(500);
         res.send({status: 500, message: error.message});
         return;
