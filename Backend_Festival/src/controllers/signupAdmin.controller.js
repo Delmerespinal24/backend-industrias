f0f9fa679b32b0d42c8a1fbf271e9806d540cf1d
carlosalgadoa <75154640+carlosalgadoa@users.noreply.github.com>
2023-04-03 22:44:42 -0600

    Nuevos campos en tabla Usuarios y actualización postman

--- Backend_Festival/src/controllers/signupAdmin.controller.js
+++ Backend_Festival/src/controllers/signupAdmin.controller.js
@@ -16,6 +16,8 @@ const addUser = async (req, res) => {
         sexo,
         password,
         esAdmin,
+        fotoPerfil,
+        fechaCreacion
       } = req.body;
 
       
@@ -29,7 +31,9 @@ const addUser = async (req, res) => {
         typeof telefono === 'undefined' ||
         typeof sexo === 'undefined' ||
         typeof password === 'undefined' ||
-        typeof esAdmin === 'undefined'
+        typeof esAdmin === 'undefined' ||
+        typeof fotoPerfil === 'undefined' 
+        //typeof fechaCreacion === 'undefined'
       ) {
         console.log('entra al if',req.body)
         return res.status(400).json({ message: `Por favor, llene todos los campos!. ${primerNombre}` });
@@ -46,7 +50,19 @@ const addUser = async (req, res) => {
         sexo,
         password,
         esAdmin,
+        fotoPerfil,
+        fechaCreacion
       };
+
+       //OBTENER LA FECHA CREACION DE USUARIO (EN QUE SE CREO EL USUARIO)
+       let hoy = new Date();
+       let dia = hoy.getDate();
+       let mes = hoy.getMonth() + 1;
+       let anio = hoy.getFullYear();
+
+      user.fechaCreacion = `${anio}-${mes}-${dia}`; 
+
+
       const connection = await getConnection();
       user.password = await helpers.encryptPassword(password);
 
