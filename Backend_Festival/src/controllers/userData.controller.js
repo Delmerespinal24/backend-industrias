353d942532c0566517ed095f0b5d7897cc4d9503
The-Running-Riot <101164427+The-Running-Riot@users.noreply.github.com>
2023-04-04 15:44:24 -0600

    UserData
    Se envian los datos en dos arreglos, un solo JSON, planes e info usuario.

--- Backend_Festival/src/controllers/userData.controller.js
+++ Backend_Festival/src/controllers/userData.controller.js
@@ -12,31 +12,25 @@ const userData = async (req, res) => {
         const rows = await pool.query('SELECT idUsuario ,primerNombre, primerApellido, nombreUsuario, fechaNacimiento, correoElectronico,'
             +'telefono, sexo, esAdmin, fechaCreacion FROM usuarios WHERE idUsuario = ?', service.decryptToken(token).sub);
 
-            const rows1 = await pool.query('SELECT idPlan ,tipoPlan, fechaInicio, fechaFin, precio, idUsuario FROM planes WHERE idUsuario = ?', service.decryptToken(token).sub);
+        const rows1 = await pool.query('SELECT idPlan ,tipoPlan, fechaInicio, fechaFin, precio, idUsuario FROM planes WHERE idUsuario = ?', service.decryptToken(token).sub);
 
-        if (rows.length > 0) {
+        if (rows.length > 0 && rows1.length > 0) {
             const user = rows[0];
-        
-            res.status(200).json({status: 200, message: "Datos del usuario obtenidos", data:rows})
-
+            const plan = rows1[0];
+            const userData = { user, plan };
+            res.status(200).json({status: 200, message: "Datos del usuario obtenidos", data: userData});
+        } else if (rows.length > 0) {
+            const user = rows[0];
+            const userData = { user, plan: null };
+            res.status(200).json({status: 200, message: "Datos del usuario obtenidos", data: userData});
         } else {
-            res.status(400).json({status: 400, message: "Este usuario no existe"})
-        }
-
-        if (rows1.length > 0) {
-            const user = rows1[0];
-        
-            res.status(200).json({status: 200, message: "Datos del usuario obtenidos", data:rows1})
-
-        } else {
-            res.status(400).json({status: 400, message: "Este usuario no posee un plan"})
+            res.status(400).json({status: 400, message: "Este usuario no existe o no posee un plan"});
         }
         
     } catch (error) {
         res.status(500);
         res.send({status: 500, message: error.message});
     }
-
 }
 
 export const methods = {
