6d5bac90aaf92f69833fcee978d12113f31ef3fb
Delmerespinal24 <Delmerespinal2@gmail.com>
2023-04-06 13:37:22 -0600

    agregando respuesta esadmin

--- Backend_Festival/src/controllers/signIn.controller.js
+++ Backend_Festival/src/controllers/signIn.controller.js
@@ -14,14 +14,14 @@ const signIn = async (req, res) => {
             return  res.status(400).json({ status: 400, message: "El usuario y la contraseña son requeridos"});
         }
         
-        const rows = await pool.query('SELECT idUsuario, password FROM usuarios WHERE nombreUsuario = ? OR correoElectronico = ?', [nombreUsuario, nombreUsuario]);
+        const rows = await pool.query('SELECT idUsuario, password, esAdmin FROM usuarios WHERE nombreUsuario = ? OR correoElectronico = ?', [nombreUsuario, nombreUsuario]);
 
         if (rows.length > 0) {
             const user = rows[0];
             const validPassword = await helpers.matchPassword(password, user.password)
             
             if (validPassword) {
-                res.status(200).send({status: 200, message: "Usted acaba de Iniciar Sesion", token: service.createToken(user) })
+                res.status(200).send({status: 200, message: "Usted acaba de Iniciar Sesion", token: service.createToken(user), esAdmin:user.esAdmin })
                 //res.status(200).json({status: 200, message: "Usted acaba de Iniciar Sesion", data:rows})
               } else {
                 res.status(400).json({status: 400, message: "Contraseña Incorrecta"})
